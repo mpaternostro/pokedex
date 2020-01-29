b0cf06fdf5b3aceeaaa09802d57d17425ac41cfd
mpaternostro <marcelodpaternostro@gmail.com>
2020-01-29 17:29:39 -0300

    Trabajo en progreso

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,5 @@
+start();
+
 listPokemons(); // List 20 pokemons by pokemon no., they will be listed on the right side of the screen.
 // BOOTSTRAP TABLE
 // You can change pages, these 20 pokemons are stored on localStorage. You can list pokemon after certain
@@ -10,18 +12,27 @@ search(); // In the input field, you can look for any pokemon you want. It shoul
 // If there's no pokemon named after that, an error should be displayed.
 // Maybe I could support habitat, color and type pokemon searches.
 
-const $toggleableBaseStats = document.querySelector('#base-stats');
+// const $toggleableBaseStats = document.querySelector('#base-stats');
 
-$toggleableBaseStats.addEventListener('click', (e) => {
-    console.log('it works');
-});
+// $toggleableBaseStats.addEventListener('click', (e) => {
+//     console.log('it works');
+// });
+
+const pokemonStats = {
+    $pokemonImage: document.querySelector('img'),
+    $pokemonName: document.querySelector('#pokemon-name'),
+    $height: document.querySelector('#height'),
+    $weight: document.querySelector('#weight'),
+    $baseStats: Array.from(document.querySelectorAll('.base-stats')),
+    $baseExperience: document.querySelector('#base-experience')
+}
 
 function search() {
     // const $select = document.querySelector('select');
-    const $input = document.querySelector('input');
-    const $button = document.querySelector('#search');
+    const $input = document.querySelector('#pokemon-tracker input');
+    const $button = document.querySelector('#pokemon-tracker button');
     // const $list = document.querySelector('ol');
-    const $pokemonImage = document.querySelector('img');
+
     // $select.addEventListener('change', (e) => {
     //     return valueToLookFor = e.target.value.toLowerCase().split(' ').join('-');
     // });
@@ -30,34 +41,49 @@ function search() {
         // const objectToSearchFor = `${valueToLookFor}-${inputValue}`;
         const cachedObject = JSON.parse(localStorage.getItem(inputValue));
         if (cachedObject) {
-            console.log('lo tengo en cache papá');
-            console.log(cachedObject);
-            const pokemonStats = {
-                $pokemonImage: document.querySelector('img'),
-                $pokemonName: document.querySelector('#pokemon-name'),
-                $height: document.querySelector('#height'),
-                $weight: document.querySelector('#weight'),
-                $baseStats: Array.from(document.querySelectorAll('.base-stats')),
-                $baseExperience: document.querySelector('#base-experience')
-            }
-            pokemonStats.$pokemonImage.src = `https://pokeres.bastionbot.org/images/pokemon/${cachedObject.id}.png`
-            pokemonStats.$pokemonName.textContent = capitalize(cachedObject.name);
-            pokemonStats.$height.textContent = `Height: ${cachedObject.height}`;
-            pokemonStats.$weight.textContent = `Weight: ${cachedObject.weight}`;
-            pokemonStats.$baseStats.forEach((value, i) => {
-                pokemonStats.$baseStats[i].textContent =
-                    `${capitalize(cachedObject.stats[i].stat.name)}: ${cachedObject.stats[i].base_stat}`;
-            });
-            pokemonStats.$baseExperience.textContent = `Base Experience: ${cachedObject.base_experience}`;
+            console.log('lo tengo en cache papá', cachedObject);
+            // pokemonStats.$pokemonImage.src = `https://pokeres.bastionbot.org/images/pokemon/${cachedObject.id}.png`
+            // pokemonStats.$pokemonName.textContent = capitalize(cachedObject.name);
+            // pokemonStats.$height.textContent = `Height: ${cachedObject.height}`;
+            // pokemonStats.$weight.textContent = `Weight: ${cachedObject.weight}`;
+            // pokemonStats.$baseStats.forEach((value, i) => {
+            //     pokemonStats.$baseStats[i].textContent =
+            //         `${capitalize(cachedObject.stats[i].stat.name)}: ${cachedObject.stats[i].base_stat}`;
+            // });
+            // pokemonStats.$baseExperience.textContent = `Base Experience: ${cachedObject.base_experience}`;
+            loadPokemonData(cachedObject);
         } else {
             fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}/`)
                 .then(response => response.json())
                 .then(responseJSON => {
-                    localStorage.setItem(`${inputValue}`, JSON.stringify(responseJSON));
-                    console.log(responseJSON);
+                    console.log('no estaba en cache, ya te lo traigo', responseJSON)
+                    localStorage.setItem(`${responseJSON.id}`, JSON.stringify(responseJSON));
+                    loadPokemonData(responseJSON);
                 });
         }
     });
+}
+
+function start() {
+    const randomPokemonID = 10;
+    randomPokemon(randomPokemonID);
+    listPokemons();
+}
+
+function randomPokemon() {
+
+}
+
+function loadPokemonData(pokeApiResponse) {
+    pokemonStats.$pokemonImage.src = `https://pokeres.bastionbot.org/images/pokemon/${pokeApiResponse.id}.png`
+    pokemonStats.$pokemonName.textContent = capitalize(pokeApiResponse.name);
+    pokemonStats.$height.textContent = `Height: ${pokeApiResponse.height}`;
+    pokemonStats.$weight.textContent = `Weight: ${pokeApiResponse.weight}`;
+    pokemonStats.$baseStats.forEach((value, i) => {
+        pokemonStats.$baseStats[i].textContent =
+            `${capitalize(pokeApiResponse.stats[i].stat.name)}: ${pokeApiResponse.stats[i].base_stat}`;
+    });
+    pokemonStats.$baseExperience.textContent = `Base Experience: ${pokeApiResponse.base_experience}`;
 }
 
 function listPokemons() {
@@ -66,7 +92,3 @@ function listPokemons() {
 
 function capitalize(string) { return string.charAt(0).toUpperCase() + string.slice(1); }
 
-function toggleAccordion() {
-    const $collapseOne = document.querySelector('#base-stats');
-    console.log($collapseOne);
-}
