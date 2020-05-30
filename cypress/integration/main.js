de0198df1f020067e574310e1dd60664a2b22679
mpaternostro <marcelodpaternostro@gmail.com>
2020-05-30 19:54:23 -0300

    actualiza pruebas cypress

--- cypress/integration/main.js
+++ cypress/integration/main.js
@@ -1,95 +1,104 @@
-// / <reference types="Cypress" />
-
-import everyPokemonName from '../../src/pokemon-names.js';
-import { getRandomPokemonName } from '../../src/pokemon-names-functions.js';
-import { capitalize } from '../../src/ui.js';
-
-const URL = 'http://127.0.0.1:8080';
+/* eslint-disable jest/expect-expect */
+// eslint-disable-next-line spaced-comment
+/// <reference types="Cypress" />
 
 describe('Poké Test', () => {
-  it('successfully loads', () => {
-    cy.visit(URL);
+  let fetchPolyfill;
+
+  before(() => {
+    const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+
+    cy.request(polyfillUrl)
+      .then((response) => {
+        fetchPolyfill = response.body;
+      });
+
+    cy.server();
+    cy.route(/https:\/\/pokeapi.co\/api\/v2\/pokemon\/\w+\//, 'fixture:bulbasaur')
+      .as('getPokemonData');
+    cy.route(/https:\/\/pokeapi.co\/api\/v2\/pokemon\/[a-z]+-[a-z]*\//, 'fixture:bulbasaur')
+      .as('getPokemonData');
+    cy.route('https://pokeapi.co/api/v2/pokemon/charmander', 'fixture:charmander')
+      .as('getPokemonData');
+    cy.route('https://pokeapi.co/api/v2/pokemon/caterpie', 'fixture:caterpie')
+      .as('getPokemonData');
+
+    cy.visit('http://127.0.0.1:8080', {
+      onBeforeLoad(contentWindow) {
+        // eslint-disable-next-line no-param-reassign
+        delete contentWindow.fetch;
+        contentWindow.eval(fetchPolyfill);
+        // eslint-disable-next-line no-param-reassign
+        contentWindow.fetch = contentWindow.unfetch;
+      },
+    });
   });
-  it('checks for spinner to appear', () => {
-    cy.get('#spinner').should('be.visible');
+
+  it('verify table is loaded with 10 pokemons', () => {
+    const firstPage = '1';
+    cy.get('#pokemon-table .pokemon-name').first()
+      .should('have.text', 'Bulbasaur');
+    cy.get('#pokemon-table .pokemon-name').last()
+      .should('have.text', 'Caterpie');
+    cy.get('#pokemon-table .pokemon-name').should('have.length', 10);
+    cy.get('.pagination .active').should('contain', firstPage);
   });
-  it('wait until a random Pokémon is loaded on the Pokédex', () => {
+
+  it('bulbasaur is loaded on the Pokédex', () => {
     cy.get('#pokedex').should('be.visible')
-      .get('#pokemon-name')
-      .should('not.be.undefined');
+      .get('#pokemon-name').should('have.text', 'Bulbasaur');
   });
-  it('clicks on "random" button and waits till a random pokemon is shown in the pokedex', () => {
-    cy.get('#pokedex #pokemon-name').then(($name) => {
-      cy.get('#random-button').click()
-        .get('#pokedex #pokemon-name')
-        .should('not.have.text', $name[0].textContent);
-    });
+
+  it('clicks on "random" button, spinner should appear and then bulbasaur should be shown again in the pokedex', () => {
+    cy.server();
+    cy.route(/https:\/\/pokeapi.co\/api\/v2\/pokemon\/\w+\//, 'fixture:bulbasaur')
+      .as('getPokemonData');
+
+    cy.get('#random-button').click();
+    cy.get('#spinner').should('be.visible');
+    cy.get('#pokedex #pokemon-name').should('have.text', 'Bulbasaur');
   });
-  const randomPokemon = capitalize(getRandomPokemonName());
-  it('looks for a random Pokémon and verify it appears in the Pokédex', () => {
-    cy.get('form').then((form$) => {
-      form$.on('submit', (e) => {
-        e.preventDefault();
-      });
-    });
-    cy.get('#search-pokemon').type(randomPokemon)
-      .next()
-      .click();
+
+  it('looks for a Pokémon and verify it appears in the Pokédex', () => {
+    cy.server();
+    cy.route('https://pokeapi.co/api/v2/pokemon/charmander', 'fixture:charmander')
+      .as('getPokemonData');
+
+    cy.get('#search-pokemon').type('Charmander');
+    cy.get('form button').click();
     cy.get('#pokedex #pokemon-name')
-      .should('have.text', randomPokemon);
+      .should('have.text', 'Charmander');
   });
+
   it('looks for a non existent Pokemon and waits for an error to appear: input should be red and Pokedex should still show last pokemon searched', () => {
-    cy.get('form').then((form$) => {
-      form$.on('submit', (e) => {
-        e.preventDefault();
-      });
-    });
-    cy.get('#search-pokemon').clear()
-      .type('pikachumon')
+    cy.get('form input').clear()
+      .type('asdasdasdasdasd')
       .next()
       .click();
     cy.get('#pokedex #pokemon-name')
-      .should('have.text', randomPokemon);
+      .should('have.text', 'Charmander');
     cy.get('#name-error').should('be.visible');
-    cy.get('#search-pokemon').should('have.class', 'alert-danger');
+    cy.get('form input').should('have.class', 'alert-danger');
   });
-  const newRandomPokemon = capitalize(getRandomPokemonName());
-  it('looks for a random Pokémon and verify it appears in the Pokédex, errors should have disappeared', () => {
-    cy.get('form').then((form$) => {
-      form$.on('submit', (e) => {
-        e.preventDefault();
-      });
-    });
-    cy.get('#search-pokemon').clear()
-      .type(newRandomPokemon)
+
+  it('looks for pokemon and verifies that it appears in the Pokédex, errors should have disappeared', () => {
+    cy.server();
+    cy.route('https://pokeapi.co/api/v2/pokemon/caterpie', 'fixture:caterpie')
+      .as('getPokemonData');
+
+    cy.get('form input').clear()
+      .type('caterpie')
       .next()
       .click();
     cy.get('#pokedex #pokemon-name')
-      .should('have.text', newRandomPokemon);
+      .should('have.text', 'Caterpie');
     cy.get('#name-error').should('not.be.visible');
     cy.get('#search-pokemon').should('not.have.class', 'alert-danger');
   });
-  it('verify table is loaded with 10 first pokemons', () => {
-    const firstPokemon = capitalize(everyPokemonName[1]);
-    const tenthPokemon = capitalize(everyPokemonName[10]);
-    const firstPage = '1';
-    cy.get('#pokemon-table .pokemon-name').first()
-      .should('have.text', firstPokemon);
-    cy.get('#pokemon-table .pokemon-name').last()
-      .should('have.text', tenthPokemon);
-    cy.get('.pagination .active').should('have.text', firstPage);
-  });
+
   it('clicks on a pokemon from the table and waits till it is shown in the pokedex', () => {
-    const seventhPokemon = capitalize(everyPokemonName[7]);
-    cy.get('#pokemon-table .pokemon-name').eq(6)
+    cy.get('#pokemon-table .pokemon-name').eq(3)
       .click();
-    cy.get('#pokedex #pokemon-name').should('have.text', seventhPokemon);
-  });
-  it('change table\'s pages, then return to page 1 and tries to click on previous button (it should be greyed out)', () => {
-    /* eslint newline-per-chained-call: ["error", { "ignoreChainWithDepth": 10 }] */
-    cy.get('.pagination').children().eq(3).click().parent().children().eq(1).click().parent().children()
-      .eq(0)
-      .click()
-      .should('have.class', 'disabled');
+    cy.get('#pokedex #pokemon-name').should('have.text', 'Charmander');
   });
 });
